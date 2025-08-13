202e92239c8e7d0cc4ece6c29fa76e7f1c9e7896
Daz <bryan@ljit.io>
2025-08-13 22:28:10 +0800

    add deep link

--- android/app/src/main/java/bw/base/root/MainActivity.java
+++ android/app/src/main/java/bw/base/root/MainActivity.java
@@ -1,10 +1,15 @@
 package bw.base.root;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
@@ -25,6 +30,42 @@ public class MainActivity extends BridgeActivity {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_CODE_POST_NOTIFICATIONS);
             }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        WebView webView = (WebView) this.bridge.getWebView();
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("https://accounts.google.com/")) {
+                    // 開外部瀏覽器跑 OAuth
+                    openOAuthInCustomTab(url);
+                    return true; // 阻止 WebView 載入
+                }
+                return false; // 繼續 WebView 正常流程
+            }
+        });
+    }
+
+    private void openOAuthInCustomTab(String url) {
+        CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Uri data = intent.getData();
+
+        String serverUrl = bridge.getServerUrl();
+
+        if (data != null && serverUrl != null && data.toString().startsWith(serverUrl)) {
+            bridge.getWebView().loadUrl(data.toString());
         }
     }
 }
